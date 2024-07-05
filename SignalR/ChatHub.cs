196a5c1ef8343b5f5f94c36c25eb91010c04998e
楊宏隆 <procedurejp@outlook.com>
2024-07-05 11:01:51 +0800

    加入專案檔案。

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR
+{
+    [HubName("ChartHub")]
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        [HubMethodName("Send")]
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+        public void Hello(string message)
+        {
+            Clients.All.HubCallBack(message);
+        }
+    }
+}
